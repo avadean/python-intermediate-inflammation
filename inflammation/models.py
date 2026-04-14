6f006689a8b2345ea8fd8bcbb70f25693952dfac
Ava Dean <ava.dean@york.ac.uk>
2026-04-14 16:38:39 +0100

    Add the Patient class

--- inflammation/models.py
+++ inflammation/models.py
@@ -32,3 +32,7 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+class Patient:
+    """A patient with a name."""
+    def __init__(self, name):
+        self.name = name
