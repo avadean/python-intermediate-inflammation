0579cb3fecaf784b66eb2fb7cd616a5be049cee6
Ava Dean <ava.dean@york.ac.uk>
2026-04-15 16:18:00 +0100

    Create a CSVDataSource class to hold the data information. Utilise the data class to load the inflammation data.

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -8,21 +8,26 @@ import numpy as np
 from inflammation import models, views
 
 
-def load_inflammation_data(data_dir):
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+class CSVDataSource:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
 
-    if not len(data_file_paths):
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
 
-    return map(models.load_csv, data_file_paths)
+        if not len(data_file_paths):
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+
+        return list(map(models.load_csv, data_file_paths))
 
 
-def analyse_data(data):
+def analyse_data(data_source: CSVDataSource):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
+    data = data_source.load_inflammation_data()
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
