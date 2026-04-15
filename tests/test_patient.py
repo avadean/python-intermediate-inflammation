5cfa4156639431e77460f33736f54d963a840608
Ava Dean <ava.dean@york.ac.uk>
2026-04-15 11:14:36 +0100

    Add patient_normalise function and test

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -1,6 +1,10 @@
 """Tests for the Patient model."""
 
 from inflammation.models import Patient
+import numpy as np
+import numpy.testing as npt
+
+from inflammation.models import patient_normalise
 
 def test_create_patient():
 
@@ -8,3 +12,13 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+def test_patient_normalise():
+    test_input = np.array([[3, 2],
+                           [3, 6],
+                           [5, 9]])
+    test_result = np.array([[1, 2 / 3],
+                           [0.5, 1],
+                           [5 / 9, 1]])
+
+    npt.assert_allclose(patient_normalise(test_input), test_result)
